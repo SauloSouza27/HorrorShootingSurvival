29d819d6fee847f287c2540744abe8d613d79830
Ian <ian.wb1@hotmail.com>
2025-03-05 12:34:56 -0300

    Beginner steps part 1

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,128 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private PlayerControls controls;
+    private CharacterController characterController;
+    private Animator animator;
+
+    [Header("Movement Info")]
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+    [SerializeField] private float gravityScale = 9.81f;
+    private float verticalVelocity;
+    private float speed;
+    private Vector3 movementDirection;
+    private bool isRunning;
+
+    [Header("Aim Info")] 
+    [SerializeField] private Transform aim;
+    [SerializeField] private LayerMask aimLayerMask;
+    private Vector3 aimDirection;
+    private Vector2 moveInput;
+    private Vector2 aimInput;
+    
+    private void Awake()
+    {
+        controls = new PlayerControls();
+
+        controls.Character.Movement.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        controls.Character.Movement.canceled += ctx => moveInput = Vector2.zero;
+        
+        controls.Character.Aim.performed += ctx => aimInput = ctx.ReadValue<Vector2>();
+        controls.Character.Aim.canceled += ctx => aimInput = Vector2.zero;
+
+        controls.Character.Run.performed += ctx =>
+        {
+            speed = runSpeed;
+            isRunning = true;
+        };
+
+        controls.Character.Run.canceled += ctx =>
+        {
+            speed = walkSpeed;
+            isRunning = false;
+        };
+    }
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+        
+        speed = walkSpeed;
+    }
+
+    private void Update()
+    {
+        ApplyMovement();
+        AimTowardsMouse();
+        AnimatorControllers();
+    }
+
+    
+    private void AnimatorControllers()
+    {
+        float xVelocity = Vector3.Dot(movementDirection.normalized, transform.right);
+        float zVelocity = Vector3.Dot(movementDirection.normalized, transform.forward);
+        
+        animator.SetFloat("xVelocity", xVelocity, .1f, Time.deltaTime);
+        animator.SetFloat("zVelocity", zVelocity, .1f, Time.deltaTime);
+        
+        animator.SetBool("isRunning", isRunning);
+    }
+
+    private void AimTowardsMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(aimInput);
+
+        if (Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, aimLayerMask))
+        {
+            aimDirection = hitInfo.point - transform.position;
+            aimDirection.y = 0f;
+            aimDirection.Normalize();
+            
+            transform.forward = aimDirection;
+            
+            aim.position = new Vector3(hitInfo.point.x, transform.position.y, hitInfo.point.z);
+        }
+    }
+
+    private void ApplyMovement()
+    {
+        movementDirection = new Vector3(moveInput.x, 0, moveInput.y);
+        ApplyGravity();
+
+        if (movementDirection.magnitude > 0)
+        {
+            characterController.Move(movementDirection * (Time.deltaTime * speed));
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        if (!characterController.isGrounded)
+        {
+            verticalVelocity -= gravityScale * Time.deltaTime;
+            movementDirection.y = verticalVelocity;
+        }
+        else
+        {
+            verticalVelocity = -.5f;
+
+        }
+    }
+
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+}
