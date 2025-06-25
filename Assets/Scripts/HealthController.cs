f3cb53c44556702caef12deb7058226404ad6541
Fabrício Frade <fabriciofrade22@gmail.com>
2025-06-24 23:00:50 -0300

    Adicionar barra de hp do jogador
    Adicionado barra de vida e script para sincronizar com a vida do jogador.
    O contador de munição foi movido de lado para ficar perto da vida do jogador.

--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -4,23 +4,29 @@ public class HealthController : MonoBehaviour
 {
     public int maxHealth = 100;
     public int currentHealth;
-    
+
+    public HealthBar healthBar;
+
     protected virtual void Awake()
     {
         currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     public virtual void ReduceHealth()
     {
         currentHealth--;
+        healthBar.SetHealth(currentHealth);
     }
 
     public virtual void IncreaseHealth()
     {
         currentHealth++;
-        
+
         if (currentHealth > maxHealth)
             currentHealth = maxHealth;
+            
+        healthBar.SetHealth(currentHealth);
     }
     
     public bool ShouldDie() => currentHealth <= 0;
