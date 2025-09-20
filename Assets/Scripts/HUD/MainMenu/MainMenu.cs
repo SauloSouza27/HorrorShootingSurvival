a8b38fddaa17bf18580f4dc89866967b4498268c
Fabrício Frade <fabriciofrade22@gmail.com>
2025-09-20 14:26:12 -0300

    Atualizar a programação da tela de menu principal e reposicionar elementos

--- Assets/Scripts/HUD/MainMenu/MainMenu.cs
+++ Assets/Scripts/HUD/MainMenu/MainMenu.cs
@@ -1,18 +1,43 @@
 using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    public void StartGame()
+    [SerializeField] private GameObject settingsPanel;
+    [SerializeField] private GameObject optionsPanel;
+    [Header("Menu UI properties")]
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button settingsButton;
+    [SerializeField] private Button quitButton;
+
+
+    private void OnEnable()
+    {
+        playButton.onClick.AddListener(StartGame);
+        settingsButton.onClick.AddListener(OpenSettingsMenu);
+        quitButton.onClick.AddListener(QuitGame);
+    }
+
+    private void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void QuitGame()
+    private void OpenSettingsMenu()
     {
+        optionsPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    private void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else   
         Application.Quit();
-        Debug.Log("Quit");
+#endif
     }
 
 }
---
+++ Assets/Scripts/HUD/SettingsUI.cs
@@ -0,0 +1,20 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsUI : MonoBehaviour
+{
+    [SerializeField] private Button backButton;
+
+    private void OnEnable()
+    {
+        backButton.onClick.AddListener(CloseSettingsMenu);
+    }
+
+    private void CloseSettingsMenu()
+    {
+        transform.parent.Find("Options").gameObject.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+}
