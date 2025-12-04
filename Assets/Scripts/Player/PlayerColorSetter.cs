3fb6315696257ceddd7ca674b83cbf205e143fb5
Saulo Souza <126815810+SauloSouza27@users.noreply.github.com>
2025-12-04 00:38:48 -0300

    Player mudar de cor

--- Assets/Scripts/Player/PlayerColorSetter.cs
+++ Assets/Scripts/Player/PlayerColorSetter.cs
@@ -3,8 +3,9 @@ using UnityEngine.InputSystem;
 
 public class PlayerColorSetter : MonoBehaviour
 {
-    [SerializeField] private Color[] playerColors;
+    [ColorUsage(true, true)] [SerializeField] private Color[] playerColors;
     [SerializeField] private int targetMaterialIndex = 0; // Set this per part in the Inspector
+    [SerializeField] private Material material;
     public int playerIndex;
     private void Start()
     {
@@ -23,13 +24,13 @@ public class PlayerColorSetter : MonoBehaviour
 
         foreach (var part in parts)
         {
-            Renderer renderer = part.GetComponent<Renderer>();
+            SkinnedMeshRenderer renderer = part.GetComponent<SkinnedMeshRenderer>();
             if (renderer == null) continue;
             
 
             MaterialPropertyBlock block = new MaterialPropertyBlock();
             renderer.GetPropertyBlock(block, part.targetMaterialIndex);
-            block.SetColor("_BaseColor", color);
+            block.SetColor("_EmissionColor", color);
             renderer.SetPropertyBlock(block, part.targetMaterialIndex);
         }
     }
