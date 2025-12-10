9d0008e35f9a486fbf6ca766f1d532ee705e0a0a
Saulo Souza <126815810+SauloSouza27@users.noreply.github.com>
2025-12-09 21:49:50 -0300

    color player upddate

--- Assets/Scripts/Player/PlayerColorSetter.cs
+++ Assets/Scripts/Player/PlayerColorSetter.cs
@@ -3,14 +3,16 @@ using UnityEngine.InputSystem;
 
 public class PlayerColorSetter : MonoBehaviour
 {
-    [ColorUsage(true, true)] [SerializeField] private Color[] playerColors;
+    [SerializeField] private Color[] playerColors;
     //[SerializeField] private int targetMaterialIndex = 0; // Set this per part in the Inspector
     [SerializeField] private Material material;
     public int playerIndex;
+    private Color colorToApply;
+
     private void Start()
     {
         playerIndex = GetComponent<PlayerInput>().playerIndex;
-        Color colorToApply = playerColors[playerIndex % playerColors.Length];
+        colorToApply = playerColors[playerIndex % playerColors.Length];
 
         ApplyColorToParts(colorToApply);
     }
@@ -30,7 +32,7 @@ public class PlayerColorSetter : MonoBehaviour
 
             MaterialPropertyBlock block = new MaterialPropertyBlock();
             renderer.GetPropertyBlock(block, part.targetMaterialIndex);
-            block.SetColor("_EmissionColor", color);
+            block.SetColor("_EmissionColor", color * 1.5f);
             renderer.SetPropertyBlock(block, part.targetMaterialIndex);
         }
     }
